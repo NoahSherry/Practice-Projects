663ddb591925ea47da5d4e839d0c1c680b9206b7
Noah Sherry <sherryn18@asmsa.org>
2017-07-01 21:22:48 -0700

    Created Puzzler

---
+++ Port Scanner/C#/PortScan/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortScan
+{
+    class Program
+    {
+        public static string IPA = "";
+        private static TcpClient tcpClient = new TcpClient();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Noah's Port Scanner");
+            Console.WriteLine("Please enter the IP address you want to scan:");
+            IPA = Console.ReadLine();
+            CheckPorts(IPA);
+
+
+            Console.ReadLine();
+        }
+
+        public static void CheckPorts(string IP)
+        {
+            for (int port = 0; port < 65535; port++)
+            {
+                    try
+                    {
+                        tcpClient.Connect(IP, port);
+                        Console.WriteLine("Port " + port + " open");
+                    }
+                    catch (Exception)
+                    {
+                    }
+            }
+        }
+    }
+}
---
+++ Port Scanner/C#/PortScan/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PortScan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("PortScan")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60261046-a001-4f92-b508-8e12133c7194")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
